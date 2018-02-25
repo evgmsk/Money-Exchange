22b5cde0d5d03c6cb868cf963c91c5f60324f49a
evg <em1777933@gmail.com>
2018-02-25 19:20:04 +0300

    make change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    const coins = ["H", "Q", "D", "N", "P"];
+    const values = [50, 25, 10, 5, 1];
+    const change = {};
+    const changeFn = (curr, i = 0) => {
+        const int = Math.floor(curr / values[i]);
+        const rem = curr % values[i];
+        if(int && curr > 0){
+            change[coins[i]] = int
+        }
+        return rem? changeFn(rem, i + 1): true;
+    };
+    changeFn(currency);
+    return currency <= 10000? change:
+        {error: "You are rich, my friend! We don't have so much coins for exchange"}
+};
